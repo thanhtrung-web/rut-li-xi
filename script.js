c7b70aa18b647f2afd1d25b7f4bc2ab7da0bcc2d
thanhtrung-web <tranthanhtrung120605@gmail.com>
2026-01-31 15:14:20 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 ﻿// Danh sách quà + tỉ lệ (%)
 const gifts = [
-    { name: "Lì Xì 2k", rate: 55 },
-    { name: "Lì Xì 5k", rate: 30 },
-    { name: "Lì Xì 10k", rate: 15 },
-    { name: "Lì Xì 20k", rate: 4 },
-    { name: "Lì Xì 50k", rate: 1 }
+    { name: "Lì Xì 2k", rate: 50},
+    { name: "Lì Xì 5k", rate: 10 },
+    { name: "Lì Xì 10k", rate: 5 },
+    { name: "Lì Xì 20k", rate: 2 },
+    { name: "1 ly soda", rate: 3 },
+    { name: "1 phần mứt chanh dây ", rate: 10 },
+    { name: "1 phần mứt mức tắc ", rate: 10 },
+    { name: "Voucher giảm 5k tổng đơn", rate: 5 },
+    { name: "Voucher giảm 7k tổng đơn", rate: 5}
 ];
 
 let isSpinning = false;
@@ -38,3 +42,4 @@ function spin() {
         isSpinning = false;
     }, 1500); // giả lập chờ 1.5s
 }
+
